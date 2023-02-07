aed1cf7042dd08478abe649a456619059c597c23
Karshiev Sardor <91384924+SardorK@users.noreply.github.com>
2023-02-07 19:50:04 +0500

    Create Project
    Add .gitignore
    Add Read.me

---
+++ InstagrammCloneApp/InstagrammCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagrammCloneApp
+//
+//  Created by User 2 on 07/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
